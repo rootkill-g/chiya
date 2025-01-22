85ac21d268577de29a109bac181eee3a52fc8f0b
rootkill-g <25330922+rootkill-g@users.noreply.github.com>
2025-01-23 01:51:55 +0530

    Added sync to workspace dependency and using sync in coroutine library, added checks for Blocker and CoroutineImpl types for non blocking

--- sync/src/atomic_option.rs
+++ sync/src/atomic_option.rs
@@ -1,5 +1,10 @@
+use std::sync::Arc;
+
 use crate::AtomicCell;
 
 pub struct AtomicOption<T> {
     inner: AtomicCell<Option<T>>,
 }
+
+const _: () = assert!(AtomicCell::<Option<CoroutineImpl>>::is_lock_free());
+const _: () = assert!(AtomicCell::<Option<Arc<Blocker>>>::is_lock_free());
