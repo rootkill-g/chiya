5214a4869d9bd0f34aa8af21a86b61a6663a0dd9
rootkill-g <25330922+rootkill-g@users.noreply.github.com>
2025-01-23 00:29:01 +0530

    Updated sync library to add primitive module with hint for spin loops, and sync module for standard and core sync module re-exports

--- sync/src/lib.rs
+++ sync/src/lib.rs
@@ -1,2 +1,20 @@
+mod atomic_cell;
+mod atomic_option;
+mod backoff;
+mod seq_lock;
 
+pub use atomic_cell::AtomicCell;
+pub use atomic_option::AtomicOption;
+pub use backoff::Backoff;
 
+#[allow(unused_imports)]
+mod primitive {
+    pub(crate) mod hint {
+        pub(crate) use core::hint::spin_loop;
+    }
+
+    pub(crate) mod sync {
+        pub(crate) use core::sync::atomic;
+        pub(crate) use std::sync::{Arc, Condvar, Mutex};
+    }
+}
