046763544bda5b086d361299d86ccdf8a25e0e9f
rootkill-g <25330922+rootkill-g@users.noreply.github.com>
2025-01-26 00:24:45 +0530

    Fixed coroutine guard

--- coroutine/src/guard.rs
+++ coroutine/src/guard.rs
@@ -1,7 +1,15 @@
 use std::ops::Range;
 
+use stack::page_size;
+
+use crate::runtime::{ContextStack, is_generator};
+
 pub type Guard = Range<usize>;
 
 pub fn current() -> Guard {
-    assert!(is_coroutine());
+    assert!(is_generator());
+
+    let guard = unsafe { (*(*ContextStack::current().root).child).stack_guard };
+
+    guard.0 - page_size()..guard.1
 }
