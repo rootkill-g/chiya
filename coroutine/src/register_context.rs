2c05b5c20f5601c22b509aabd146ba8b865c1400
rootkill-g <25330922+rootkill-g@users.noreply.github.com>
2025-01-26 16:43:42 +0530

    Importing the renamed Register type for RegisterContext

--- coroutine/src/register_context.rs
+++ coroutine/src/register_context.rs
@@ -1,15 +1,15 @@
-use crate::stack::Registers;
+use crate::stack::Register;
 
 #[derive(Debug)]
 pub struct RegisterContext {
     /// Hold the registeres while the task or scheduler is suspended
-    pub(crate) regs: Registers,
+    pub(crate) regs: Register,
 }
 
 impl RegisterContext {
     pub fn empty() -> RegisterContext {
         RegisterContext {
-            regs: Registers::new(),
+            regs: Register::new(),
         }
     }
 }
