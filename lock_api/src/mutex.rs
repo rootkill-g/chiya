faa16a44f04e20fec2c94dc77a048dad861124f0
rootkill-g <25330922+rootkill-g@users.noreply.github.com>
2025-01-27 12:25:54 +0530

    Added implementation for Mutex with RawMutex lock

--- lock_api/src/mutex.rs
+++ lock_api/src/mutex.rs
@@ -23,7 +23,33 @@ pub struct Mutex<R, T: ?Sized> {
     data: UnsafeCell<T>,
 }
 
-unsafe impl<R: RawMutex + Send, T: ?Sized> Send for Mutex<R, T> {}
-unsafe impl<R: RawMutex + Sync, T: ?Sized> Sync for Mutex<R, T> {}
+unsafe impl<R: RawMutex + Send, T: ?Sized + Send> Send for Mutex<R, T> {}
+unsafe impl<R: RawMutex + Sync, T: ?Sized + Send> Sync for Mutex<R, T> {}
 
+impl<R: RawMutex, T> Mutex<R, T> {
+    /// Creates a new mutex in an unlocked state ready for use
+    #[cfg(has_const_fn_trait_bound)]
+    #[inline]
+    pub const fn new(value: T) -> Mutex<R, T> {
+        Mutex {
+            raw: R::INIT,
+            data: UnsafeCell::new(value),
+        }
+    }
+
+    /// Creates a new mutex in an unlocked state ready for use
+    #[cfg(not(has_const_fn_trait_bound))]
+    #[inline]
+    pub fn new(value: T) -> Mutex<R, T> {
+        Mutex {
+            raw: R::INIT,
+            data: UnsafeCell::new(value),
+        }
+    }
+
+    /// Consumes this mutex, returning the underlying data
+    #[inline]
+    pub fn into_inner(self) -> T {
+        self.data.into_inner()
+    }
 }
