1eea2f24d7cd741676c7feac7839b5c8242fe409
rootkill-g <25330922+rootkill-g@users.noreply.github.com>
2025-01-27 23:54:13 +0530

    Added newly created modules, mutex_guard & arc_mutex_guard, added alloc as extern crate under arc_lock feature

--- lock_api/src/lib.rs
+++ lock_api/src/lib.rs
@@ -3,8 +3,13 @@
 #![no_std]
 #![warn(missing_docs)]
 
+mod arc_mutex_guard;
 mod mutex;
+mod mutex_guard;
 mod raw_mutex;
 mod raw_mutex_fair;
 mod raw_mutex_timed;
 mod rwlock;
+
+#[cfg(feature = "arc_lock")]
+extern crate alloc;
